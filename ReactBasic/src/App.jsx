14f0bb01b3c53c303f6d496702a7f5cd8ecce9a1
russel <russeljeoff2@gmail.com>
2026-04-25 22:50:00 +0800

    add Body component and update App structure

--- ReactBasic/src/App.jsx
+++ ReactBasic/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Header.jsx'
 import Footer from './footer.jsx'
 import Food from './Food.jsx'
+import Body from './Body.jsx'
 function App() {
   return(
     <>
       <Header></Header>
+      <Body></Body>
       <Food></Food>
       <Footer></Footer>
     </>
