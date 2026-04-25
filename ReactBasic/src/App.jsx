167bc93da4ae8b773797c289ddf8d91622b1ea59
russel <russeljeoff2@gmail.com>
2026-04-25 15:47:30 +0800

    first commit

---
+++ ReactBasic/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Header.jsx'
+import Footer from './footer.jsx'
+import Food from './Food.jsx'
+function App() {
+  return(
+    <>
+      <Header></Header>
+      <Food></Food>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
